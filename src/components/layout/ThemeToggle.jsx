7519964a23d4efa32d9b4144734bd48bb41b5a15
Subham Nayak <sn343555@gmail.com>
2025-12-29 21:59:14 +0530

    Update ThemeToggle.jsx

--- src/components/layout/ThemeToggle.jsx
+++ src/components/layout/ThemeToggle.jsx
@@ -3,21 +3,6 @@ import { useTheme } from '../../contexts/ThemeContext';
 
 const ThemeToggle = () => {
   const { theme, toggleTheme } = useTheme();
-  
-
-  console.log('ThemeToggle Component:');
-  console.log('Theme from context:', theme);
-  console.log('toggleTheme function:', typeof toggleTheme);
-  
-
-  if (!theme || !toggleTheme) {
-    console.error('Theme context not available!');
-    return (
-      <button className="p-2 rounded-lg bg-red-500 text-white">
-         Theme Error
-      </button>
-    );
-  }
 
   return (
     <button
